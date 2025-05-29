4623dd31d329bd1c9bf763e054e43e29f25ccf62
Samuel <vinhhien1012001@gmail.com>
2025-05-29 17:44:04 +0400

    feat: add desktop side bar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,16 @@
-import React from "react";
 import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "components";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
       MobileSidebar
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
       <aside className="children">
         <Outlet />
       </aside>
